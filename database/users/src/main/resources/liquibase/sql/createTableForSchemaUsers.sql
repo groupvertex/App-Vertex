6d97285667e956c942b8e5bb00858793405d07a0
Vasyl <leo124@bigmir.net>
2016-05-17 16:32:39 +0300

    try

---
+++ database/users/src/main/resources/liquibase/sql/createTableForSchemaUsers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users.register_user(
+  id         SERIAL      NOT NULL CONSTRAINT first_key PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name  VARCHAR(50) NOT NULL,
+  email      VARCHAR(50) NOT NULL,
+  password   VARCHAR(50) NOT NULL
+);
