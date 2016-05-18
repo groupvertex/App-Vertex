3cc3ff7a5823d411c62c2ecb4eabdc770b670e97
Vasya <leo124@bigmir.net>
2016-05-18 17:41:54 +0300

    Waypoint (#14)
    * waypoint
    
    * waypoint updated

---
+++ database/routes/src/main/java/ua/vertex/waypoint/DAO/WayPointDAO.java
@@ -0,0 +1,16 @@
+package ua.vertex.waypoint.DAO;
+
+import ua.vertex.waypoint.Entity.WayPoint;
+
+import java.util.List;
+
+/**
+ * Created by Vasyl on 18/05/2016.
+ */
+public interface WayPointDAO {
+    int createWayPoint(WayPoint wayPoint);
+    WayPoint readWayPoint(int id);
+    void updateWayPoint(int id, WayPoint wayPoint);
+    void deleteWayPoint(int id);
+    List<WayPoint> getSortedWayPointsForRoute(int routeId);
+}
