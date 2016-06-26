2a2bb4cbbbf7a9fc9e6ebab0c705cec2ad2f6229
Vitalik <s.vitalii314@gmail.com>
2016-06-26 22:51:55 +0300

    modified

--- ls/ls-fe/src/main/java/ua/vertex/config/Application.java
+++ ls/ls-fe/src/main/java/ua/vertex/config/Application.java
@@ -2,10 +2,8 @@ package ua.vertex.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("rws")
 public class Application {
 
     public static void main(String[] args) {
