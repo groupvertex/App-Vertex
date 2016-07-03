0c858adf059fcef6f1c50c97408f0b9ad659b4e0
user <mail>
2016-07-03 10:48:27 +0300

    Adding archaius

---
+++ configManager/src/test/java/com/vertex/config/prims/$booleanTest.java
@@ -0,0 +1,23 @@
+package com.vertex.config.prims;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class $booleanTest {
+
+    @Before
+    public void setUp() {
+        System.setProperty("testProperty", "true");
+
+    }
+
+    @Test
+    public void shouldCorrectlyProcessBooleanValues() {
+        $boolean prop = new $boolean("testProperty", false);
+
+        assertEquals(true, prop.get());
+    }
+
+}
