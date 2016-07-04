ab682544bfaf786246db300d6c2fc5b034de335c
vitalii314 <s.vitalii314@gmail.com>
2016-07-04 15:59:16 +0300

    Rws be client (#36)
    * rws
    
    * rws
    
    * rws
    
    * rws

---
+++ rws/rws-fe/src/main/java/ua.vertex/Application.java
@@ -0,0 +1,22 @@
+package ua.vertex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Nick on 7/2/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+
+}
