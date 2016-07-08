122e188ce944b0a0b3ea4495757819206beab295
Rostislav <yeaxi98@gmail.com>
2016-07-08 20:06:40 +0300

    spring cloud and actuator were added

--- rws/rws-be/src/main/java/ua/vertex/RouteController.java
+++ rws/rws-be/src/main/java/ua/vertex/RouteController.java
@@ -1,42 +1,41 @@
-package my;
+package ua.vertex;
 
 import entity.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 import ua.vertex.dao.route.RouteDAO;
 
-//@Component
 @RestController
-@RequestMapping("/route")
+@RequestMapping("/routes")
+@RefreshScope
 public class RouteController {
-
 
     @Autowired
     private RouteDAO routeDAO;
 
 
-    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Route getRoute(@PathVariable long id) {
         return routeDAO.read(id);
 
     }
 
-    @RequestMapping( method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public void addRoute(@RequestBody Route route) {
         routeDAO.create(route);
 
     }
 
-    @RequestMapping( value="/{id}", method = RequestMethod.PUT)
-    public void updateRoute(@PathVariable long id , @RequestBody Route route) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void updateRoute(@PathVariable long id, @RequestBody Route route) {
         routeDAO.update(route, id);
     }
 
-    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteRoute(@PathVariable long id) {
         routeDAO.delete(id);
     }
-
 
 
 }
